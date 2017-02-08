17d28151a25d42e4734c5b7e9d10f66fa9751d5f
LimaDani <danieleddelima@gmail.com>
2017-02-08 17:15:31 -0200

    Binary heap - C++

---
+++ Binary-heap/binaryHeap.cpp
@@ -0,0 +1,124 @@
+/*----------------------------------------------------------
+*	CENTRO FEDERAL DE EDUCACAO TECNOLOGICA DE MINAS GERAIS
+*	LABORATORIO DE ALGORITMOS E ESTRUTURAS DE DADOS II
+*   TRABALHO PRATICO III  
+*   1.2 HEAP BINARIO  
+*-----------------------------------------------------------*/
+#include "binaryHeap.hpp"
+
+Heap::Heap(int cap)
+{
+    size = 0;
+    capacity = cap;
+    harr = new int[cap];
+}
+ 
+
+void Heap::insert(int n)
+{
+    if (size == capacity)
+    {
+        cout << "\n Overflow \n";
+        return;
+    }
+
+    size++;
+    int i = size - 1;
+    harr[i] = n;
+ 
+    while (i != 0 && harr[pai(i)] > harr[i])
+    {
+       swap(&harr[i], &harr[pai(i)]);
+       i = pai(i);
+    }
+}
+ 
+
+void Heap::decrease(int i, int new_val)
+{
+    harr[i] = new_val;
+    while (i != 0 && harr[pai(i)] > harr[i])
+    {
+       swap(&harr[i], &harr[pai(i)]);
+       i = pai(i);
+    }
+}
+ 
+
+int Heap::extractMin()
+{
+    if (size <= 0)
+        return INT_MAX;
+    if (size == 1)
+    {
+        size--;
+        return harr[0];
+    }
+ 
+    int root = harr[0];
+    harr[0] = harr[size-1];
+    size--;
+    MinHeapify(0);
+ 
+    return root;
+}
+ 
+
+void Heap::remove(int i)
+{
+    decrease(i, INT_MIN);
+    extractMin();
+}
+ 
+
+void Heap::MinHeapify(int i)
+{
+    int l = left(i);
+    int r = right(i);
+    int menor = i;
+    if (l < size && harr[l] < harr[i])
+        menor = l;
+    if (r < size && harr[r] < harr[menor])
+        menor = r;
+    if (menor != i)
+    {
+        swap(&harr[i], &harr[menor]);
+        MinHeapify(menor);
+    }
+}
+ 
+
+void Heap::swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+ 
+int main()
+{
+    Heap h(11);
+    h.insert(3);
+    h.insert(2);
+    h.remove(1);
+    h.insert(15);
+    h.insert(5);
+    h.insert(4);
+    h.insert(45);
+	cout << endl << endl;
+    cout << "Remove o elemento mínimo do Heap: ";
+	cout << h.extractMin() << endl;
+    cout << "Elemento raiz do Heap: "; 
+	cout << h.getMin() << endl << endl ;
+	cout << "Diminui o valor de chave. " << endl << endl;
+    h.decrease(2, 1);
+    cout << "Novo elemento raiz do Heap: ";
+	cout << h.getMin() << endl << endl;
+
+    return 0;
+}
+
+
+
+
---
+++ Binary-heap/binaryHeap.hpp
@@ -0,0 +1,43 @@
+/*----------------------------------------------------------
+*	CENTRO FEDERAL DE EDUCACAO TECNOLOGICA DE MINAS GERAIS
+*	LABORATORIO DE ALGORITMOS E ESTRUTURAS DE DADOS II
+*   TRABALHO PRATICO III  
+*   1.2 HEAP BINARIO  
+*-----------------------------------------------------------*/
+#ifndef BINARYHEAP_H
+#define BINARYHEAP_H
+#include<climits>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Heap
+{
+    int *harr;
+    int capacity; 
+    int size;
+	void swap(int *x, int *y); 
+public:
+
+    Heap(int capacity);
+
+    void MinHeapify(int );
+
+    int pai(int i) { return (i-1)/2; }
+ 
+    int left(int i) { return (2*i + 1); }
+ 
+    int right(int i) { return (2*i + 2); }
+ 
+    int extractMin();
+ 
+    void decrease(int i, int new_val);
+
+    int getMin(){ return harr[0]; }
+
+    void remove(int i);
+
+    void insert(int k);
+
+    void heapsort( );
+};
+#endif
